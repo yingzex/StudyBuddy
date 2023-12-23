1312e1a98a337990d3537602d1e1cddd85574fd4
xyz <15109268895x@gmail.com>
2023-12-22 19:49:32 -0500

    edit user account page
    Signed-off-by: xyz <15109268895x@gmail.com>

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User 
 from django.contrib import messages 
@@ -172,3 +172,16 @@ def deleteMessage(request, pk):
         return redirect('home') 
 
     return render(request, 'base/delete.html', {'obj':message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
+    if request.method=='POST':
+        form=UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    return render(request, 'base/update-user.html', {'form':form})
+
